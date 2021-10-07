7bf781fa568de39714494f5ef77ac9afec3cdade
Jakub Cichocki <cichykanal32@gmail.com>
2021-10-07 15:56:15 +0100

    second try

--- Program.cs
+++ Program.cs
@@ -2,11 +2,44 @@
 
 namespace ConsoleApp1
 {
-    class Program
+
+    class Cylinder
+    {
+        private double height;
+        private double radius;
+
+        public Cylinder(double r, double h)
+        {
+            height = h;
+            radius = r;
+        }
+
+        public double VolumeCalculation()
+        {
+            return 3.142 * radius * radius * height;
+        }
+
+        public double getHeight()
+        {
+            return (height);
+        }
+
+        public double getRadius()
+        {
+            return (radius);
+        }
+
+    }
+
+
+    class TestingClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Worlddsds!");
+            Console.WriteLine("Cylinder");
+            Cylinder c = new Cylinder(2, 3);  
+            Console.WriteLine(c.VolumeCalculation());
+
         }
     }
 }
